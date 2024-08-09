7039bceb43b044c78143f09075cb4d91eb69ede8
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-09 14:31:16 +0200

    refactor: wrapping task into component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import './App.css';
-import { useState} from "react";
+import {useState} from "react";
+import {Task} from "./Task";
 
 function App() {
 
-    const [toDoList,setToDoList] = useState([]);
-    const [newTask,setNewTask] = useState("");
+    const [toDoList, setToDoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
 
-    const handleInput = (event) =>{
+    const handleInput = (event) => {
         setNewTask(event.target.value);
     }
 
-    const addTaskToList = () =>{
-        const task ={
-            id: toDoList.length ==0 ? 1: toDoList[toDoList.length-1].id+1,
+    const addTaskToList = () => {
+        const task = {
+            id: toDoList.length == 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
             name: newTask,
         }
-        setToDoList([...toDoList,task]);
+        setToDoList([...toDoList, task]);
     }
 
     const deleteTask = (taskId) => {
-        setToDoList(toDoList.filter((task) => task.id!== taskId));
+        setToDoList(toDoList.filter((task) => task.id !== taskId));
     };
 
 
     return (
-       <div className="App">
+        <div className="App">
 
-           <div className="addTask">
-               <input onChange={handleInput}/>
-               <button onClick={addTaskToList}>Add Task</button>
-           </div>
+            <div className="addTask">
+                <input onChange={handleInput}/>
+                <button onClick={addTaskToList}>Add Task</button>
+            </div>
 
-           <div className="list">
-               {toDoList.map((task) =>{
-                   return (
-                       <div>
-                       <h1>{task.name}</h1>
-                       <button onClick={()=> deleteTask(task.id)}>X</button>
-                       </div>
-                   )
-               })}
-           </div>
+            <div className="list">
+                {toDoList.map((task) => {
+                    return (
+                        <Task name={task.name} id={task.id} deleteTask={deleteTask}/>
+                    )
+                })}
+            </div>
 
 
-
-       </div>
+        </div>
     )
 }
-
-
 
 
 export default App;
